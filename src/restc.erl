999d9929584312277873beb8e63cd560c1ae09f0
Daniel Widgren <daniel.widgren@kivra.com>
2012-05-22 18:20:12 +0200

    Fixed so that parse_body can handle empty binary

--- src/restc.erl
+++ src/restc.erl
@@ -127,6 +127,7 @@ parse_response({error, Type}) ->
 
 parse_body([], Body)                 -> Body;
 parse_body(_, [])                    -> [];
+parse_body(_, <<>>)                  -> [];
 parse_body("application/json", Body) -> jsx:to_term(Body);
 parse_body("application/xml", Body)  ->
     {ok, Data, _} = erlsom:simple_form(binary_to_list(Body)),
