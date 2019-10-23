98acc673832939957e504f97fb7aa5efead42d89
Alexandre Rodrigues <alexandre@kivra.com>
2019-10-23 16:44:38 +0200

    Add .elvis to gitignore

--- src/restc.erl
+++ src/restc.erl
@@ -37,7 +37,14 @@
 -export([construct_url/2]).
 -export([construct_url/3]).
 
--type method()       :: head | get | put | patch | post | trace | options | delete.
+-type method()       :: head    |
+                        get     |
+                        put     |
+                        patch   |
+                        post    |
+                        trace   |
+                        options |
+                        delete.
 -type url()          :: binary() | string().
 -type headers()      :: [header()].
 -type header()       :: {binary(), binary()}.
@@ -50,198 +57,224 @@
 -type reason()       :: term().
 -type content_type() :: json | xml | percent | png.
 -type body()         :: binary() | jsx:json_term() | erlsom:simple_form().
--type response()     :: {ok, Status::status_code(), Headers::headers(), Body::body()} |
-                        {error, Status::status_code(), Headers::headers(), Body::body()} |
-                        {error, Reason::reason()}.
+-type response()     :: { ok, Status::status_code()
+                        , Headers::headers()
+                        , Body::body()}               |
+                        { error, Status::status_code()
+                        , Headers::headers()
+                        , Body::body()}               |
+                        { error, Reason::reason()}.
 
 -define(DEFAULT_ENCODING, json).
 -define(DEFAULT_CTYPE, <<"application/json">>).
 
--export_type([
-     method/0
-    ,url/0
-    ,headers/0
-    ,header/0
-    ,options/0
-    ,option/0
-    ,querys/0
-    ,qry/0
-    ,status_codes/0
-    ,status_code/0
-    ,reason/0
-    ,content_type/0
-    ,body/0
-    ,response/0
-]).
+-export_type([method/0
+             , url/0
+             , headers/0
+             , header/0
+             , options/0
+             , option/0
+             , querys/0
+             , qry/0
+             , status_codes/0
+             , status_code/0
+             , reason/0
+             , content_type/0
+             , body/0
+             , response/0
+             ]).
 
 %%% API ========================================================================
 
 
 -spec request(Url::url()) -> Response::response().
 request(Url) ->
-    request(get, Url).
+  request(get, Url).
 
 -spec request(Method::method(), Url::url()) -> Response::response().
 request(Method, Url) ->
-    request(Method, Url, []).
+  request(Method, Url, []).
 
--spec request(Method::method(), Url::url(), Expect::status_codes()) -> Response::response().
+-spec request(Method::method(),
+              Url::url(),
+              Expect::status_codes()) -> Response::response().
 request(Method, Url, Expect) ->
-    request(Method, ?DEFAULT_ENCODING, Url, Expect).
+  request(Method, ?DEFAULT_ENCODING, Url, Expect).
 
--spec request(Method::method(), Type::content_type(), Url::url(),
+-spec request(Method::method(),
+              Type::content_type(),
+              Url::url(),
               Expect::status_codes()) -> Response::response().
 request(Method, Type, Url, Expect) ->
-    request(Method, Type, Url, Expect, []).
+  request(Method, Type, Url, Expect, []).
 
--spec request(Method::method(), Type::content_type(), Url::url(),
-              Expect::status_codes(), Headers::headers()) -> Response::response().
+-spec request(Method::method(),
+              Type::content_type(),
+              Url::url(),
+              Expect::status_codes(),
+              Headers::headers()) -> Response::response().
 request(Method, Type, Url, Expect, Headers) ->
-    request(Method, Type, Url, Expect, Headers, []).
+  request(Method, Type, Url, Expect, Headers, []).
 
--spec request(Method::method(), Type::content_type(), Url::url(),
-              Expect::status_codes(), Headers::headers(), Body::body()) -> Response::response().
+-spec request(Method::method(),
+              Type::content_type(),
+              Url::url(),
+              Expect::status_codes(),
+              Headers::headers(),
+              Body::body()) -> Response::response().
 request(Method, Type, Url, Expect, Headers, Body) ->
-    request(Method, Type, Url, Expect, Headers, Body, []).
+  request(Method, Type, Url, Expect, Headers, Body, []).
 
--spec request(Method::method(), Type::content_type(), Url::url(),
-    Expect::status_codes(), Headers::headers(), Body::body(), Options::options()) -> Response::response().
+-spec request(Method::method(),
+              Type::content_type(),
+              Url::url(),
+              Expect::status_codes(),
+              Headers::headers(),
+              Body::body(),
+              Options::options()) -> Response::response().
 request(Method, Type, Url, Expect, Headers0, Body, Options) ->
-    Headers1 = normalize_headers(Headers0),
-    Headers = lists:usort([ accept(Headers1, Type), content_type(Headers1, Type) | Headers1]),
-    Retries = proplists:get_value(retries, Options, 0),
-    request_loop(Method, Type, Url, Expect, Headers, Body, Options, Retries).
+  Headers1 = normalize_headers(Headers0),
+  Headers = lists:usort([ accept(Headers1, Type)
+                        , content_type(Headers1, Type) | Headers1]),
+  Retries = proplists:get_value(retries, Options, 0),
+  request_loop(Method, Type, Url, Expect, Headers, Body, Options, Retries).
 
 request_loop(Method, Type, Url, Expect, Headers, Body, Options, Retries) ->
-    Response = parse_response(do_request(Method, Type, Url, Headers, Body, Options)),
-    case Response of
-        {ok, Status, H, B} ->
-            case check_expect(Status, Expect) of
-                true -> Response;
-                false when Retries > 0 ->
-                    request_loop(Method, Type, Url, Expect, Headers, Body, Options, Retries-1);
-                false ->
-                    {error, Status, H, B}
-            end;
-        _Error when Retries > 0 ->
-            request_loop(Method, Type, Url, Expect, Headers, Body, Options, Retries-1);
-        Error ->
-            Error
-    end.
+  Response =
+    parse_response(do_request(Method, Type, Url, Headers, Body, Options)),
+  case Response of
+    {ok, Status, H, B} ->
+      case check_expect(Status, Expect) of
+        true -> Response;
+        false when Retries > 0 ->
+          request_loop(Method, Type, Url, Expect, Headers, Body, Options,
+                       Retries-1);
+        false ->
+          {error, Status, H, B}
+      end;
+    _Error when Retries > 0 ->
+      request_loop(Method, Type, Url, Expect, Headers, Body, Options,
+                   Retries-1);
+    Error ->
+      Error
+  end.
 
 -spec construct_url(FullPath::url(), Query::querys()) -> Url::url().
 construct_url(FullPath, Query) when is_binary(FullPath) ->
-    construct_url(binary_to_list(FullPath), Query);
+  construct_url(binary_to_list(FullPath), Query);
 construct_url(FullPath, Query) when is_list(FullPath) ->
-    {S, N, P, _, _} = mochiweb_util:urlsplit(FullPath),
-    Q = mochiweb_util:urlencode(Query),
-    mochiweb_util:urlunsplit({S, N, P, Q, []}).
+  {S, N, P, _, _} = mochiweb_util:urlsplit(FullPath),
+  Q = mochiweb_util:urlencode(Query),
+  mochiweb_util:urlunsplit({S, N, P, Q, []}).
 
--spec construct_url(FullPath::url(), Path::url(), Query::querys()) -> Url::url().
+-spec construct_url(FullPath::url(),
+                    Path::url(),
+                    Query::querys()) -> Url::url().
 construct_url(SchemeNetloc, Path, Query) when is_binary(SchemeNetloc) ->
-    construct_url(binary_to_list(SchemeNetloc), Path, Query);
+  construct_url(binary_to_list(SchemeNetloc), Path, Query);
 construct_url(SchemeNetloc, Path, Query) when is_binary(Path) ->
-    construct_url(SchemeNetloc, binary_to_list(Path), Query);
+  construct_url(SchemeNetloc, binary_to_list(Path), Query);
 construct_url(SchemeNetloc, Path, Query) when is_list(SchemeNetloc),
                                               is_list(Path) ->
-    {S, N, P1, _, _} = mochiweb_util:urlsplit(SchemeNetloc),
-    {_, _, P2, _, _} = mochiweb_util:urlsplit(Path),
-    P = path_cat(P1, P2),
-    urlunsplit(S, N, P, Query).
+  {S, N, P1, _, _} = mochiweb_util:urlsplit(SchemeNetloc),
+  {_, _, P2, _, _} = mochiweb_util:urlsplit(Path),
+  P = path_cat(P1, P2),
+  urlunsplit(S, N, P, Query).
 
 %%% INTERNAL ===================================================================
 normalize_headers(Headers) ->
-    lists:map(fun({Key, Val}) ->
-                      {string:lowercase(Key), Val}
-              end, Headers).
+  lists:map(fun({Key, Val}) ->
+                {string:lowercase(Key), Val}
+            end, Headers).
 
 accept(Headers, Type) ->
-    case lists:keyfind(<<"accept">>, 1, Headers) of
-        {<<"accept">>, Accept} -> {<<"accept">>, Accept};
-        false -> default_accept(Type)
-    end.
+  case lists:keyfind(<<"accept">>, 1, Headers) of
+    {<<"accept">>, Accept} -> {<<"accept">>, Accept};
+    false -> default_accept(Type)
+  end.
 
 default_accept(Type) ->
-    AccessType = get_accesstype(Type),
-    {<<"accept">>, <<AccessType/binary, ", */*;q=0.9">>}.
+  AccessType = get_accesstype(Type),
+  {<<"accept">>, <<AccessType/binary, ", */*;q=0.9">>}.
 
 content_type(Headers, Type) ->
-    case lists:keyfind(<<"content-type">>, 1, Headers) of
-        {<<"content-type">>, ContentType} -> {<<"content-type">>, ContentType};
-        false -> default_content_type(Type)
-    end.
+  case lists:keyfind(<<"content-type">>, 1, Headers) of
+    {<<"content-type">>, ContentType} -> {<<"content-type">>, ContentType};
+    false -> default_content_type(Type)
+  end.
 
 default_content_type(Type) ->
-    {<<"content-type">>, get_ctype(Type)}.
+  {<<"content-type">>, get_ctype(Type)}.
 
 do_request(post, Type, Url, Headers, Body, Options) ->
-    Body2 = encode_body(Type, Body),
-    hackney:request(post, Url, Headers, Body2, Options);
+  Body2 = encode_body(Type, Body),
+  hackney:request(post, Url, Headers, Body2, Options);
 do_request(put, Type, Url, Headers, Body, Options) ->
-    Body2 = encode_body(Type, Body),
-    hackney:request(put, Url, Headers, Body2, Options);
+  Body2 = encode_body(Type, Body),
+  hackney:request(put, Url, Headers, Body2, Options);
 do_request(patch, Type, Url, Headers, Body, Options) ->
-    Body2 = encode_body(Type, Body),
-    hackney:request(patch, Url, Headers, Body2, Options);
+  Body2 = encode_body(Type, Body),
+  hackney:request(patch, Url, Headers, Body2, Options);
 do_request(Method, _, Url, Headers, _, Options) when is_atom(Method) ->
-    hackney:request(Method, Url, Headers, [], Options).
+  hackney:request(Method, Url, Headers, [], Options).
 
 check_expect(_Status, []) ->
-    true;
+  true;
 check_expect(Status, Expect) ->
-    lists:member(Status, Expect).
+  lists:member(Status, Expect).
 
 encode_body(json, Body) ->
-    jsx:encode(Body);
+  jsx:encode(Body);
 encode_body(percent, Body) ->
-    mochiweb_util:urlencode(Body);
+  mochiweb_util:urlencode(Body);
 encode_body(xml, Body) ->
-    lists:flatten(xmerl:export_simple(Body, xmerl_xml)).
+  lists:flatten(xmerl:export_simple(Body, xmerl_xml)).
 
 urlunsplit(S, N, P, Query) ->
-    Q = mochiweb_util:urlencode(Query),
-    mochiweb_util:urlunsplit({S, N, P, Q, []}).
+  Q = mochiweb_util:urlencode(Query),
+  mochiweb_util:urlunsplit({S, N, P, Q, []}).
 
 path_cat(P1, P2) ->
-    UL = lists:append(path_fix(P1), path_fix(P2)),
-    ["/"++U || U <- UL].
+  UL = lists:append(path_fix(P1), path_fix(P2)),
+  ["/"++U || U <- UL].
 
 path_fix(S) ->
-    PS = mochiweb_util:path_split(S),
-    path_fix(PS, []).
+  PS = mochiweb_util:path_split(S),
+  path_fix(PS, []).
 
 path_fix({[], []}, Acc) ->
-    lists:reverse(Acc);
+  lists:reverse(Acc);
 path_fix({[], T}, Acc) ->
-    path_fix(mochiweb_util:path_split(T), Acc);
+  path_fix(mochiweb_util:path_split(T), Acc);
 path_fix({H, T}, Acc) ->
-    path_fix(mochiweb_util:path_split(T), [H|Acc]).
+  path_fix(mochiweb_util:path_split(T), [H|Acc]).
 
 parse_response({ok, 204, Headers, Client}) ->
-    ok = hackney:close(Client),
-    {ok, 204, Headers, []};
+  ok = hackney:close(Client),
+  {ok, 204, Headers, []};
 parse_response({ok, Status, Headers, Client}) ->
-    NormalizedHeaders = normalize_headers(Headers),
-    {<<"content-type">>, ContentType} = content_type(NormalizedHeaders, ?DEFAULT_CTYPE),
-    Type = parse_type(ContentType),
-    case hackney:body(Client) of
-        {ok, Body}   -> {ok, Status, Headers, parse_body(Type, Body)};
-        {error, _}=E -> E
-    end;
+  NormalizedHeaders = normalize_headers(Headers),
+  {<<"content-type">>, ContentType} =
+    content_type(NormalizedHeaders, ?DEFAULT_CTYPE),
+  Type = parse_type(ContentType),
+  case hackney:body(Client) of
+    {ok, Body}   -> {ok, Status, Headers, parse_body(Type, Body)};
+    {error, _}=E -> E
+  end;
 parse_response({error, Type}) ->
-    {error, Type}.
+  {error, Type}.
 
 parse_type(Type) ->
-    case binary:split(Type, <<";">>) of
-        [CType, _] -> CType;
-        _ -> Type
-    end.
+  case binary:split(Type, <<";">>) of
+    [CType, _] -> CType;
+    _ -> Type
+  end.
 
 parse_body(_, <<>>)                      -> [];
 parse_body(<<"application/json">>, Body) -> jsx:decode(Body);
 parse_body(<<"application/xml">>, Body)  ->
-    {ok, Data, _} = erlsom:simple_form(binary_to_list(Body)),
-    Data;
+  {ok, Data, _} = erlsom:simple_form(binary_to_list(Body)),
+  Data;
 parse_body(<<"text/xml">>, Body)  -> parse_body(<<"application/xml">>, Body);
 parse_body(<<"image/png">>, Body) -> Body;
 parse_body(_, Body)               -> Body.
@@ -257,3 +290,9 @@ get_ctype(xml)     -> <<"application/xml">>;
 get_ctype(percent) -> <<"application/x-www-form-urlencoded">>;
 get_ctype(png)     -> <<"image/png">>;
 get_ctype(_)       -> get_ctype(?DEFAULT_ENCODING).
+
+%%%_* Emacs ============================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
