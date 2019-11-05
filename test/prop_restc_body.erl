2a43fe8bff0a6690c9422f741fa721d7c44955df
Alexandre Rodrigues <alexandre@kivra.com>
2019-11-05 16:56:06 +0100

    test: Add test of encoding/decoding json body

---
+++ test/prop_restc_body.erl
@@ -0,0 +1,21 @@
+-module(prop_restc_body).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_decode_encode_json() ->
+    ?FORALL(Obj, object_proplist(),
+            Obj =:= restc_body:decode(<<"application/json">>, restc_body:encode(json, Obj))).
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
+object_proplist() ->
+    list({key(), value()}).
+
+key() ->
+    utf8().
+
+value() ->
+    oneof([utf8(), integer()]).
